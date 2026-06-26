1eb8285f3b974a9a6eaa977d74ea60be57398b2a
Mercè Amich <170412173+merceamvi@users.noreply.github.com>
2026-06-26 09:10:23 +0200

    update metadata + last analysis script
    Updated the last run date and modified the last analysis stage script to '04_profiles_ames.R'.

--- 00_run_all.R
+++ 00_run_all.R
@@ -1,23 +1,28 @@
 ###############################################################################
 # Project       : [rehabSpain] [00] MASTER RUNNER
 # Creation date : 09/06/2026
-# Last update   : 09/06/2026
+# Last update   : 22/06/2026
 # Author        : Mercè Amich (merce.amich@ehu.eus)
 # Institution   : UPV/EHU, BC3
+
 # Last run time : 4.20 h. aprox.
 #   - Script 01: 1.96 min.
 #   - Script 02: 5.90 min.
 #   - Script 239.4 min. (4h aprox.)
 #   - Script 04: 5.0 min.
+
 # Script Overview:
 #   Runs the full pipeline in order. Each stage saves its intermediate
 #   objects to disk, so stages can also be run individually.
+
 # Requirements:
 #   - All stage scripts and _setup.R in the same directory
 #   - HDD_CDD.xlsx (for stage 01)
 #   - Internet access for stage 01 (INE download); skip 01 if data.RData
 #     is already present in the directory.
+
 # Output  : data.RData + intermediate .RData + publication tables/figures
+
 ###############################################################################
 
 rm(list = ls(all = TRUE))
@@ -30,6 +35,6 @@ source("01_build_data.R")
 # Analysis stages (each loads the previous stage's intermediate objects)
 source("02_descriptives_logit.R")
 source("03_famd_clustering.R")
-source("04_profiles_policy.R")
+source("04_profiles_ames.R")
 
 cat("PIPELINE COMPLETE")
